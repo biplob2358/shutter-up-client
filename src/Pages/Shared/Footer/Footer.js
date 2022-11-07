772bd50e5e5e33f692d4b481fa9e172beef27bd7
unknown <biplob2358@gmail.com>
2022-11-08 01:09:02 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.jpg";
 
 const Footer = () => {
   return (
     <div>
-      <h2>This is footer</h2>
+      <footer className="footer p-10 bg-sky-100 text-base-content">
+        <div>
+          <img className="w-24" src={logo} alt="logo" />
+          <p>
+            Shutter Up
+            <br />
+            Providing reliable Photoshoot since 2020
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">Services</span>
+          <Link to="/" className="link link-hover">
+            Branding
+          </Link>
+          <Link className="link link-hover">Design</Link>
+          <Link className="link link-hover">Marketing</Link>
+          <Link className="link link-hover">Advertisement</Link>
+        </div>
+
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link to="/" className="link link-hover">
+            Terms of use
+          </Link>
+          <Link to="/" className="link link-hover">
+            Privacy policy
+          </Link>
+          <Link to="/" className="link link-hover">
+            Cookie policy
+          </Link>
+        </div>
+      </footer>
     </div>
   );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -24,39 +24,41 @@ const Header = () => {
   );
   return (
     <div>
-      <div className="navbar bg-sky-100 rounded font-bold">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+      <div className="navbar bg-sky-100 rounded font-bold  ">
+        <div className="container mx-auto">
+          <div className="navbar-start ">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {manuItems}
-            </ul>
+                {manuItems}
+              </ul>
+            </div>
+            <Link to="/" className="btn btn-ghost normal-case text-xl">
+              <img className="w-12 h-12" src={logo} alt="logo" />
+              <p className="text-3xl px-2">Shutter Up</p>
+            </Link>
           </div>
-          <Link to="/" className="btn btn-ghost normal-case text-xl">
-            <img className="w-12 h-12" src={logo} alt="logo" />
-            <p className="text-4xl px-2">Shutter Up</p>
-          </Link>
-        </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">{manuItems}</ul>
+          <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">{manuItems}</ul>
+          </div>
         </div>
       </div>
     </div>
