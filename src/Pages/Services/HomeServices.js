5a69e782a9f284f6557c5183d558f43553d0204d
unknown <biplob2358@gmail.com>
2022-11-08 04:15:57 +0600

    cart updated

--- src/Pages/Services/HomeServices.js
+++ src/Pages/Services/HomeServices.js
@@ -4,13 +4,15 @@ import ServiceCart from "../ServicesCart/ServiceCart";
 
 const HomeServices = () => {
   const servicesHome = useLoaderData();
+  const sliceService = servicesHome.slice(0, 3);
+
   return (
     <div>
       <div className="text-center">
         <p className="text-2xl font-bold text-orange-600">Services</p>
         <h2 className="text-5xl font-semibold">Current Packages</h2>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 g-4 ">
-          {servicesHome.map((service) => (
+          {sliceService.map((service) => (
             <ServiceCart key={service._id} service={service}></ServiceCart>
           ))}
         </div>
